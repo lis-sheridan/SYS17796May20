746cbc1af34bbd23d231a8fe482dd677068deeca
lis-sheridan <shanli@sheridancollege.ca>
2025-05-13 13:25:01 -0400

    2nd changes

--- src/ca/sheridancollege/week1/softwarefundamentals/StudentList.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/StudentList.java
@@ -15,11 +15,21 @@ public class StudentList {
     
     public static void main(String[] args)
     {
-       Student [] studentList = new Student[1];//change size before running
+       Student [] studentList = new Student[0];//change size before running
        Student s1 = new Student();
-       s1.setName("Liz Dancy");
+       s1.setName("Li");
        studentList[0]=s1;
-       //fill in 4 student names before running
+       //fill in rest 3 student names before running
+       Student s2 = new Student();
+       s2.setName("Farook");
+       studentList[1]=s2;
+       Student s3 = new Student();
+       s3.setName("Julia");
+       studentList[2]=s3;
+       Student s4 = new Student();
+       s4.setName("Sophie");
+       studentList[3]=s4;
+              
        for(Student s: studentList)
        {
            System.out.println(s.getName());
